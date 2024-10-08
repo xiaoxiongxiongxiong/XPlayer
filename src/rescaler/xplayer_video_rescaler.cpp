965d0c6a6aa1ba8f8676571f6f413701e0e5e722
小熊熊熊 <981303787@qq.com>
2024-10-08 21:17:33 +0800

    实现视频转换器功能

---
+++ src/rescaler/xplayer_video_rescaler.cpp
@@ -0,0 +1,146 @@
+﻿#include "xplayer_video_rescaler.h"
+#include "utils/xplayer_utils.h"
+
+bool CXPlayerVideoRescaler::create(const AVPixelFormat in_fmt, const int in_width, const int in_height,
+                                   const AVPixelFormat out_fmt, const int out_width, const int out_height)
+{
+    if (_rescaler)
+    {
+        xpu_format_string(_err, "rescaler has already opened");
+        return false;
+    }
+
+    // 判断像素格式是否支持
+    if (!sws_isSupportedInput(in_fmt))
+    {
+        xpu_format_string(_err, "Input pixel:%d format is not supported", in_fmt);
+        return false;
+    }
+    if (!sws_isSupportedOutput(out_fmt))
+    {
+        xpu_format_string(_err, "Output pixel:%d format is not supported.", out_fmt);
+        return false;
+    }
+
+    // 完全相同，不需要转换
+    if (in_width == out_width && in_height == out_height && in_fmt == out_fmt)
+    {
+        xpu_format_string(_err, "No need swscale!");
+        _need_rescale = false;
+        return true;
+    }
+
+    _rescaler = sws_getContext(in_width, in_height, in_fmt, out_width, out_height, out_fmt, 0, nullptr, nullptr, nullptr);
+    if (nullptr == _rescaler)
+    {
+        xpu_format_string(_err, "sws_alloc_context failed!");
+        return false;
+    }
+
+    int ret = av_image_alloc(_out_data, _out_linesize, out_width, out_height, out_fmt, 64);
+    if (ret <= 0)
+    {
+        char buff[AV_ERROR_MAX_STRING_SIZE] = { 0 };
+        av_make_error_string(buff, AV_ERROR_MAX_STRING_SIZE, ret);
+        xpu_format_string(_err, "av_image_alloc failed, err:%s", buff);
+        sws_freeContext(_rescaler);
+        _rescaler = nullptr;
+        return false;
+    }
+
+    _in_width = in_width;
+    _in_height = in_height;
+    _in_fmt = in_fmt;
+    _out_width = out_width;
+    _out_height = out_height;
+    _out_fmt = out_fmt;
+    _need_rescale = true;
+
+    return true;
+}
+
+void CXPlayerVideoRescaler::destroy()
+{
+    if (nullptr != _rescaler)
+    {
+        sws_freeContext(_rescaler);
+        _rescaler = nullptr;
+    }
+
+    if (nullptr != _out_data)
+    {
+        av_freep(&_out_data[0]);
+        memset(_out_data, 0, sizeof(_out_data));
+        memset(_out_linesize, 0, sizeof(_out_linesize));
+    }
+
+    _in_width = 0;
+    _in_height = 0;
+    _out_width = 0;
+    _out_height = 0;
+    _need_rescale = true;
+}
+
+bool CXPlayerVideoRescaler::rescale(const AVFrame * in_frm, AVFrame * out_frm)
+{
+    if (nullptr == in_frm || nullptr == in_frm->data[0] || 0 >= in_frm->linesize[0] || nullptr == out_frm)
+    {
+        xpu_format_string(_err, "Input param is invalid!");
+        return false;
+    }
+
+    if (!_need_rescale)
+    {
+        memcpy(out_frm->data, in_frm->data, sizeof(in_frm->data[0]) * AV_NUM_DATA_POINTERS);
+        memcpy(out_frm->linesize, in_frm->linesize, sizeof(in_frm->linesize[0]) * AV_NUM_DATA_POINTERS);
+        return true;
+    }
+
+    if (nullptr == _rescaler)
+    {
+        xpu_format_string(_err, "rescaler is not open!");
+        return false;
+    }
+
+    int ret = sws_scale(_rescaler, in_frm->data, in_frm->linesize, 0, _in_height, _out_data, _out_linesize);
+    if (ret <= 0)
+    {
+        xpu_format_string(_err, "sws_scale failed!");
+        return false;
+    }
+
+    copyFrame(out_frm, in_frm);
+
+    return true;
+}
+
+const char * CXPlayerVideoRescaler::err() const
+{
+    return _err.c_str();
+}
+
+void CXPlayerVideoRescaler::copyFrame(AVFrame * dst_frm, const AVFrame * src_frm)
+{
+    if (nullptr == src_frm || nullptr == dst_frm)
+    {
+        xpu_format_string(_err, "Input param is nullptr!");
+        return;
+    }
+
+    // 拷贝数据
+    memcpy(dst_frm->data, _out_data, sizeof(_out_data[0]) * AV_NUM_DATA_POINTERS);
+    memcpy(dst_frm->linesize, _out_linesize, sizeof(_out_linesize[0]) * AV_NUM_DATA_POINTERS);
+
+    // 拷贝参数
+    dst_frm->format = static_cast<int>(_out_fmt);
+    dst_frm->pts = dst_frm->pts;
+    dst_frm->pkt_dts = src_frm->pkt_dts;
+    dst_frm->duration = src_frm->duration;
+    dst_frm->width = _out_width;
+    dst_frm->height = _out_height;
+    dst_frm->color_range = src_frm->color_range;
+    dst_frm->color_primaries = src_frm->color_primaries;
+    dst_frm->color_trc = src_frm->color_trc;
+    dst_frm->color_range = src_frm->color_range;
+    dst_frm->colorspace = src_frm->colorspace;
+}
---
+++ src/rescaler/xplayer_video_rescaler.h
@@ -0,0 +1,68 @@
+﻿#ifndef __XPLAYER_VIDEO_RESCALER_H__
+#define __XPLAYER_VIDEO_RESCALER_H__
+
+#include <cstdio>
+#include <cstdint>
+#include <cstdbool>
+#include <string>
+
+extern "C" {
+#include "libavformat/avformat.h"
+#include "libswscale/swscale.h"
+#include "libavutil/imgutils.h"
+}
+
+class CXPlayerVideoRescaler
+{
+public:
+    CXPlayerVideoRescaler() = default;
+    ~CXPlayerVideoRescaler() = default;
+
+    // 创建转换器
+    bool create(AVPixelFormat in_fmt, int in_width, int in_height, AVPixelFormat out_fmt, int out_width, int out_height);
+
+    // 销毁
+    void destroy();
+
+    // 转换
+    bool rescale(const AVFrame * in_frm, AVFrame * out_frm);
+
+    // 错误信息
+    const char * err() const;
+
+protected:
+    // 拷贝数据帧
+    void copyFrame(AVFrame * dst_frm, const AVFrame * src_frm);
+
+private:
+    // 是否需要转换
+    bool _need_rescale = true;
+
+    // 输入宽度
+    int _in_width = 0;
+    // 输入高度
+    int _in_height = 0;
+
+    // 输出高度
+    int _out_width = 0;
+    // 输出宽度
+    int _out_height = 0;
+
+    // 输入像素格式
+    AVPixelFormat _in_fmt = AVPixelFormat::AV_PIX_FMT_NONE;
+    // 输出像素格式
+    AVPixelFormat _out_fmt = AVPixelFormat::AV_PIX_FMT_NONE;
+
+    // 输出数据
+    uint8_t * _out_data[AV_NUM_DATA_POINTERS] = { nullptr };
+    // 输出行
+    int _out_linesize[AV_NUM_DATA_POINTERS] = { 0 };
+
+    // 视频转换器
+    SwsContext * _rescaler = nullptr;
+
+    // 错误信息
+    std::string _err;
+};
+
+#endif
