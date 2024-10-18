67e9ef9bffa5a598e6c7094f08e47f53f0572050
小熊熊熊 <981303787@qq.com>
2024-10-18 21:39:41 +0800

    实现基于SDL的音频渲染器

--- src/renderer/xplayer_audio_render.cpp
+++ src/renderer/xplayer_audio_render.cpp
@@ -1,1 +1,99 @@
 ﻿#include "xplayer_audio_render.h"
+
+#include "SDL2/SDL_audio.h"
+
+#include "utils/xplayer_utils.h"
+
+struct _audio_info_t
+{
+    int volume;
+    int len;
+    int pos;         // 播放位置
+    uint8_t * data;
+};
+
+static void audio_render_cb(void * udata, uint8_t * data, int len)
+{
+    auto * audio_info = reinterpret_cast<audio_info_t *>(udata);
+    if (nullptr == audio_info || 0 == audio_info->len)
+    {
+        return;
+    }
+
+    SDL_memset(data, 0, len);
+    int tmp_len = len > audio_info->len ? audio_info->len : len;
+
+    SDL_MixAudioFormat(data, audio_info->data + audio_info->pos, AUDIO_S16, tmp_len, audio_info->volume);
+    audio_info->pos += tmp_len;
+    audio_info->len -= tmp_len;
+}
+
+bool CXPlayerAudioRender::create(int sample_rate, int channels, int frame_size, int vol)
+{
+    _cache = static_cast<audio_info_t *>(calloc(1, sizeof(audio_info_t)));
+    if (nullptr == _cache)
+    {
+        xpu_format_string(_err, "No enough memory");
+        return false;
+    }
+    _cache.load()->volume = vol;
+
+    SDL_AudioSpec audio_player = {};
+    audio_player.freq = sample_rate;
+    audio_player.format = AUDIO_S16SYS;
+    audio_player.channels = channels;
+    audio_player.silence = 0;
+    audio_player.samples = frame_size;
+    audio_player.callback = audio_render_cb;
+    audio_player.userdata = _cache;
+    if (SDL_OpenAudio(&audio_player, nullptr) < 0)
+    {
+        xpu_format_string(_err, "SDL_OpenAudio failed!");
+        free(_cache);
+        _cache = nullptr;
+        return false;
+    }
+    SDL_PauseAudio(0);
+
+    return true;
+}
+
+void CXPlayerAudioRender::destroy()
+{
+    SDL_CloseAudio();
+
+    if (nullptr != _cache)
+    {
+        free(_cache.load());
+        _cache = nullptr;
+    }
+}
+
+void CXPlayerAudioRender::setVolume(int vol)
+{
+    if (nullptr != _cache)
+        return;
+    _cache.load()->volume = vol;
+}
+
+void CXPlayerAudioRender::mute(bool flag)
+{
+    SDL_PauseAudio(flag);
+}
+
+void CXPlayerAudioRender::renderer(uint8_t * data, int len)
+{
+    if (nullptr != _cache)
+    {
+        _cache.load()->data = data;
+        _cache.load()->len = len;
+        _cache.load()->pos = 0;
+    }
+}
+
+bool CXPlayerAudioRender::finished()
+{
+    if (nullptr != _cache)
+        return _cache.load()->len <= 0;
+    return true;
+}
--- src/renderer/xplayer_audio_render.h
+++ src/renderer/xplayer_audio_render.h
@@ -1,11 +1,41 @@
 ﻿#ifndef __XPLAYER_AUDIO_RENDER_H__
 #define __XPLAYER_AUDIO_RENDER_H__
 
+#include <cstdbool>
+#include <cstdint>
+#include <atomic>
+#include <string>
+
+typedef struct _audio_info_t audio_info_t;
+
 class CXPlayerAudioRender
 {
 public:
     CXPlayerAudioRender() = default;
     ~CXPlayerAudioRender() = default;
+
+    // 创建
+    bool create(int sample_rate, int channels, int frame_size, int vol);
+    // 销毁
+    void destroy();
+
+    // 设置音量
+    void setVolume(int vol);
+
+    // 是否静音
+    void mute(bool flag);
+
+    // 渲染
+    void renderer(uint8_t * data, int len);
+
+    // 是否完毕
+    bool finished();
+
+private:
+    std::atomic<audio_info_t *> _cache = { nullptr };
+
+    // 错误信息
+    std::string _err;
 };
 
 #endif
