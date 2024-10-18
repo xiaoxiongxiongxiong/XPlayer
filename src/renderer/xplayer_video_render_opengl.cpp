cef2fa655fa5d9b4627669033d2622b49bf2954b
小熊熊熊 <981303787@qq.com>
2024-10-18 21:41:38 +0800

    搭建基于opengl的视频渲染器框架

---
+++ src/renderer/xplayer_video_render_opengl.cpp
@@ -0,0 +1,36 @@
+﻿#include "xplayer_video_render_opengl.h"
+
+CXPlayerVideoRender::CXPlayerVideoRender(QWidget * parent) :
+    QOpenGLWidget(parent)
+{
+
+}
+
+void CXPlayerVideoRender::initializeGL()
+{
+
+}
+
+void CXPlayerVideoRender::paintGL()
+{
+}
+
+void CXPlayerVideoRender::resizeGL(int w, int h)
+{
+
+}
+
+void CXPlayerVideoRender::paintEvent(QPaintEvent * event)
+{
+    //QPainter painter(this);
+    //painter.setPen(Qt::NoPen); // 关闭默认的绘图边框模式
+    //painter.setBrush(Qt::white); // 设置底色为黑色
+    //painter.drawRect(0, 0, width() - 1, height() - 1); // 绘制矩形，除最后一个像素
+
+    //// 绘制底边框
+    //painter.setPen(QPen(Qt::white, 2)); // 设置蓝色边框，宽度为2
+    //painter.drawLine(0, height() - 2, width(), height() - 2); // 绘制底边框
+
+    //// 调用基类函数绘制子部件
+    //QOpenGLWidget::paintEvent(event);
+}
---
+++ src/renderer/xplayer_video_render_opengl.h
@@ -0,0 +1,23 @@
+﻿#ifndef __XPLAYER_VIDEO_RENDER_OPENGL_H__
+#define __XPLAYER_VIDEO_RENDER_OPENGL_H__
+
+#include <QOpenglWidget>
+#include <QOpenGLFunctions_3_0>
+#include <QPainter>
+
+class CXPlayerVideoRender :public QOpenGLWidget, protected QOpenGLFunctions_3_0
+{
+    Q_OBJECT
+public:
+    CXPlayerVideoRender(QWidget * parent = nullptr);
+    ~CXPlayerVideoRender() = default;
+
+protected:
+    void initializeGL() override;
+    void paintGL() override;
+    void resizeGL(int w, int h) override;
+
+    void paintEvent(QPaintEvent * event) override;
+};
+
+#endif
